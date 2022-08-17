616a2bdc3a75ed04d9dde1dc003931eb30fb3f45
piyazilim <48718431+piyazilim@users.noreply.github.com>
2022-08-17 13:41:17 +0300

    oddishOrEvenish.js
    Oddish Or Evenish Project

---
+++ oddishOrEvenish.js
@@ -0,0 +1,19 @@
+const number = prompt("Enter a number: ");
+
+var total = 0,
+    sNumber = number.toString();
+    
+oddishOrEvenish(sNumber);
+
+function oddishOrEvenish(sNumber){
+	for (var i = 0, len = sNumber.length; i < len; i += 1) {
+    total += Number(sNumber[i]);
+    
+	}
+  if(total % 2 == 0) 
+     console.log("The number is even.");
+ 
+ if(total % 2 == 1) 
+     console.log("The number is odd.");
+}
+    
